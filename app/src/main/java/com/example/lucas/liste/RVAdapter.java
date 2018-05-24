35716a784cb36a7a657af1c41d94e54c8155a609
Lucas Reynolds <lreynolds18@gmail.com>
2018-05-24 17:38:50 -0400

    Got shopping list activity to have recyclerview that is populated by crypto currency names from coinmarketcap restapi

---
+++ app/src/main/java/com/example/lucas/liste/RVAdapter.java
@@ -0,0 +1,81 @@
+package com.example.lucas.liste;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class RVAdapter extends RecyclerView.Adapter<RVAdapter.ViewHolder> {
+
+    private ArrayList<String> mData;
+    private LayoutInflater mInflater;
+    private ItemClickListener mClickListener;
+
+    // data is passed into the constructor
+    RVAdapter(Context context, ArrayList<String> data) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = data;
+    }
+
+    // inflates the row layout from xml when needed
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.recyclerview_row, parent, false);
+        return new ViewHolder(view);
+    }
+
+    // binds the data to the TextView in each row
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        String item = mData.get(position);
+        holder.myTextView.setText(item);
+    }
+
+    // total number of rows
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+
+    // stores and recycles views as they are scrolled off screen
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView myTextView;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            myTextView = itemView.findViewById(R.id.item);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
+        }
+    }
+
+    // convenience method for getting data at click position
+    String getItem(int id) {
+        return mData.get(id);
+    }
+
+    // convenience method to add multiple data points
+    void setItems(ArrayList<String> mData) {
+        mData.addAll(mData);
+        this.notifyDataSetChanged();
+    }
+
+    // allows clicks events to be caught
+    void setClickListener(ItemClickListener itemClickListener) {
+        this.mClickListener = itemClickListener;
+    }
+
+    // parent activity will implement this method to respond to click events
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
+    }
+}
--- app/src/main/java/com/example/lucas/liste/ShoppingListActivity.java
+++ app/src/main/java/com/example/lucas/liste/ShoppingListActivity.java
@@ -2,13 +2,12 @@ package com.example.lucas.liste;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.loopj.android.http.JsonHttpResponseHandler;
 
@@ -17,38 +16,56 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import cz.msebera.android.httpclient.Header;
 
-public class ShoppingListActivity extends AppCompatActivity {
+public class ShoppingListActivity extends AppCompatActivity
+        implements RVAdapter.ItemClickListener {
 
     private Button mainActivityButton;
     private ArrayList<String> values;
     private RecyclerView rv;
+    private RVAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        values = new ArrayList<>();
-        rv = findViewById(R.id.shopping_list_rv);
-        getCrypto();
         setContentView(R.layout.activity_shopping_list);
 
-        mainActivityButton = findViewById(R.id.btn_main_activity);
+        values = new ArrayList<>();
+        rv = (RecyclerView) findViewById(R.id.shopping_list_rv);
+        getCrypto();
+
+        mainActivityButton = (Button) findViewById(R.id.btn_main_activity);
         mainActivityButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(ShoppingListActivity.this, MainActivity.class));
             }
         });
+
+        // ArrayList<String> al = new ArrayList<>();
+        // al.add("item1");
+        // al.add("item2");
+        // al.add("item3");
+
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new RVAdapter(this, values);
+        adapter.setClickListener(this);
+        rv.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
     }
 
     public void callUpdate() {
-        for (int i=0; i<values.size(); i++) {
-            System.out.println(values.get(i));
-        }
+        adapter.setItems(values);
+        // adapter.notifyDataSetChanged();
+//        for (int i=0; i<values.size(); i++) {
+//            System.out.println(values.get(i));
+//        }
     }
 
     public void getCrypto() {
